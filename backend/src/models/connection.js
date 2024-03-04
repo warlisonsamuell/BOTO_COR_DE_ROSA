a2b7ab56256830ce2eba1dcb482668e540f7f9f2
victorguivasco <victorguilhermecavalcante@gmail.com>
2024-03-04 02:32:41 -0400

    feat: create getAll to get all information from tables "pessoa" and "formacao"

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -2,12 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const Connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-})
-
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
 
 // Conectar ao banco de dados
 Connection.connect((err) => {
